d1238707817af5068be92532b46497481ce3c5d9
elifoguzkan <oguzkanelif@gmail.com>
2021-01-21 01:34:51 +0300

    ClassMetotDemo
    Testing

---
+++ MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Yeni Müşteri Eklendi: "  + musteri.Id + musteri.Ad + musteri.Soyad + musteri.Bakiye );
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi: " +musteri.Id + musteri.Ad + musteri.Soyad + musteri.Bakiye);
+        }
+    }
+}
